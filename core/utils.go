09bc0c39d6c108d4b4a398f8995b2e1e06bb43b9
Leo Spratt <enchant97@users.noreply.github.com>
2024-06-14 11:56:09 +0100

    ensure generated slug only uses ascii alphanumerical

--- core/utils.go
+++ core/utils.go
@@ -5,10 +5,12 @@ import (
 	"encoding/base64"
 )
 
+var RandomSlugEncoding = base64.NewEncoding("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+
 func GenerateRandomSlug(length uint) string {
 	rBytes := make([]byte, length)
 	if _, err := rand.Read(rBytes); err != nil {
 		panic(err)
 	}
-	return base64.RawURLEncoding.EncodeToString(rBytes)[:length]
+	return RandomSlugEncoding.EncodeToString(rBytes)[:length]
 }
